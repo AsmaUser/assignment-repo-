ac47c9a8d38c10899efdc395973786820333a616
Asma Siyabi <asma.alsiyabi2000@gmail.com>
2023-11-26 21:13:56 +0400

    Update quetion 10

--- assignment2/Q10.py
+++ assignment2/Q10.py
@@ -1,16 +1,8 @@
-def filter_long_words(word_list, n):
-    longer_words = [word for word in word_list if len(word) > n]
-    return longer_words
+import random
+import string
+n = int (input ("Enter a size: "))
+def get_random_string(length):
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    print(result_str)
 
-# Get input from the user
-words_input = input("Enter a list of words separated by spaces: ")
-threshold_length_input = int(input("Enter the threshold length: "))
-
-# Split the input string into a list of words
-words_list = words_input.split()
-
-# Call the function with user inputs
-result = filter_long_words(words_list, threshold_length_input)
-
-# Print the result
-print("Words longer than {threshold_length_input} characters: {result}")
+get_random_string(n)
